ffb17a8d8b10cf47ccfc31e86432db83ae300251
SpadeFater <113428597+SpadeFater@users.noreply.github.com>
2025-11-08 13:55:36 +0800

    first commit

---
+++ src/AppWindow.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+public class AppWindow extends JFrame  {    //创建一个窗口
+	public static void main(String[] args) {		
+       
+	    JFrame frame = new AppWindow();         //创建一个窗口
+        JPanel panel = new JPanel();
+
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //设置一个关闭窗口         
+	    frame.setSize(1280 , 900);         //setSize()设置窗口大小         
+	    frame.setVisible(true);           //setVisible(true)  让窗口显示出来	}
+        frame.setTitle("游戏助手");
+        frame.setBackground(Color.BLUE);
+
+        frame.add(panel); //将面板添加到窗口中
+        panel.setBackground(Color.YELLOW);
+        }
+    }
